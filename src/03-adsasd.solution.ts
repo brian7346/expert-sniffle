7fef5d8989254f7e6ace64363152c20479ae2d94
brian7346 <doctorbrian97@gmail.com>
2025-04-22 17:07:24 +0400

    initial

---
+++ src/03-adsasd.solution.ts
@@ -0,0 +1,15 @@
+/*
+ * @metadata {
+ *   "title": "Adsasd",
+ *   "descriptionVideoId": "",
+ *   "solutionVideoId": ""
+ * }
+ */
+
+// Ваше задание здесь
+// Опишите что нужно сделать
+
+export const solution() {
+  // Здесь должно быть решение
+  return true;
+}
